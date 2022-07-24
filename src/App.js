789ea37cd91b005b88c8c7cd6c0fac85383304b4
Brit Wallace <britwallace888@gmail.com>
2022-07-24 15:56:22 -0700

    update match logic

--- src/App.js
+++ src/App.js
@@ -1,20 +1,23 @@
 
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import './App.css'
 import SingleCard from './components/SingleCard'
+import { cardImages, allTarotImages} from './components/FullDeck'
 
-const cardImages = [
-  { "src": "/images/c01.jpg" },
-  { "src": "/images/c02.jpg" },
-  { "src": "/images/c03.jpg" },
-  { "src": "/images/c04.jpg" },
-  { "src": "/images/c05.jpg" },
-  { "src": "/images/c06.jpg" },
-]
+// const cardImages = [
+//   { "src": "/images/c01.jpg" },
+//   { "src": "/images/c02.jpg" },
+//   { "src": "/images/c03.jpg" },
+//   { "src": "/images/c04.jpg" },
+//   { "src": "/images/c05.jpg" },
+//   { "src": "/images/c06.jpg" },
+// ]
 
 function App() {
   const [cards, setCards] = useState([])
   const [turns, setTurns] = useState(0)
+  const [choiceOne, setChoiceOne] = useState(null)
+  const [choiceTwo, setChoiceTwo] = useState(null)
 
   // shuffle cards for new game
   const shuffleCards = () => {
@@ -26,7 +29,45 @@ function App() {
     setTurns(0)
   }
 
-  console.log(cards, turns)
+  // console.log(cards, turns)
+
+
+  // handle a choice
+  const handleChoice = (card) => {
+    console.log(card)
+    choiceOne ? setChoiceTwo(card) : setChoiceOne(card)
+  }
+
+  // compare 2 selected cards
+  useEffect(() => {
+    if (choiceOne && choiceTwo) {
+
+      if (choiceOne.src === choiceTwo.src) {
+        setCards(prevCards => {
+          return prevCards.map(card => {
+            if (card.src === choiceOne.src) {
+              return { ...card, matched: true }
+            } else {
+              return card
+            }
+          })
+        })
+        resetTurn()
+      } else {
+        resetTurn()
+      }
+
+    }
+  }, [choiceOne, choiceTwo])
+
+  console.log(cards)
+
+  // reset choices and increase turn 
+  const resetTurn = () => {
+    setChoiceOne(null)
+    setChoiceTwo(null)
+    setTurns(prevTurns => prevTurns + 1 )
+  }
 
   return (
     <div className="App">
@@ -38,6 +79,7 @@ function App() {
           <SingleCard
             key={card.id}
             card={card}
+            handleChoice={handleChoice}
             />
         ))}
       </div>
@@ -46,4 +88,4 @@ function App() {
   );
 }
 
-export default App
+export default App;
--- src/components/SingleCard.js
+++ src/components/SingleCard.js
@@ -1,11 +1,20 @@
 import './SingleCard.css'
 
-export default function SingleCard({ card }) {
+export default function SingleCard({ card, handleChoice }) {
+
+  const handleClick = () => {
+    handleChoice(card)
+  }
+  
+  
   return (
     <div className="card">
       <div>
           <img className="front" src={card.src} alt="card front"/>
-          <img className="back" src="/images/tarot-card-back.jpg" alt="card front"/>
+          <img className="back" 
+          src="/images/tarot-card-back.jpg"
+          onClick={handleClick} 
+          alt="card front"/>
       </div>
     </div>
   )
